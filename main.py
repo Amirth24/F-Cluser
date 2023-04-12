3061969c994ff902b295e3de9674b55529906960
Amirth24 <amirth300324@gmail.com>
2023-04-12 22:36:04 +0530

    Add file uploading

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-
-
-
-
 st.set_page_config(
     layout='wide',
     page_icon='👁️'
@@ -11,5 +7,13 @@ st.set_page_config(
 
 
 
+def run_process():
+    global uploaded_files
+    if len(uploaded_files) <= 1:
+        st.error('Upload more than one file to process.', icon='⚠️')
+    else:
+        st.write('No of files:', len(uploaded_files))
 
 uploaded_files = st.sidebar.file_uploader('Upload Images', accept_multiple_files=True, type=["png","jpg","jpeg"])
+
+process_btn = st.sidebar.button('Process', on_click=run_process)
